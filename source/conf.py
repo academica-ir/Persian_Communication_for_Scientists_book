b23ddd523cacb4aea6bef364af58a83db5eef5ea
academica-ir <abmeedu@yahoo.com>
2025-05-12 11:33:10 +0330

    اولین انتشار

---
+++ source/conf.py
@@ -0,0 +1,24 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+
+project = 'ارتباط علمی برای پژوهشگران'
+author = 'ترجمه و گردآوری'
+release = '1.0'
+
+extensions = ['myst_parser']
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'fa'
+html_static_path = ['_static']
+html_theme = 'pydata_sphinx_theme'
+
+html_theme_options = {
+    "navigation_with_keys": True,
+    "show_nav_level": 2,
+}
+
+html_css_files = [
+    'rtl.css',
+]
